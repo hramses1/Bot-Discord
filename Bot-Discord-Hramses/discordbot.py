89b337df2ed20b9399ed35bb9e7c4c59b7c9a8e3
hramses1 <79066311+hramses1@users.noreply.github.com>
2021-05-25 15:06:44 -0500

    Update discordbot.py

--- Bot-Discord-Hramses/discordbot.py
+++ Bot-Discord-Hramses/discordbot.py
@@ -57,60 +57,12 @@ async def vete(ctx):
     voz = get(bot.voice_clients,guild=ctx.guild)
     await voz.disconnect()
 # --------------------------------------------------------
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-# --------------------------------------------------------
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        # loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-client = commands.Bot(command_prefix='?')
-
-status = ['Jamming out to music!', 'Eating!', 'Sleeping!']
-
 @bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='general')
     await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `?help` command for details!')
-@bot.command(pass_context=True)
-async def play1(ctx,*, search):
+@bot.command(pass_context=True, name='play', help='This command plays music')
+async def play(ctx,*, search):
     # --------------------------------------------------------
     try:
         canal = ctx.message.author.voice.channel
@@ -292,26 +244,7 @@ async def lista(ctx,*,search):
     await ctx.send("Añadida la cancion nro "+str(Lista_num)+" a la lista de Reproduccion")
     print("Cancion añadida")
 # --------------------------------------------------------
-@bot.command(name='play', help='This command plays music')
-async def play(ctx,*, url):
-
-    query_string= parse.urlencode({'search_query':url})
-    html_content = request.urlopen('https://www.youtube.com/results?' + query_string)
-    search_results = re.findall( r"watch\?v=(\S{11})", html_content.read().decode())
-    # await ctx.send("https://www.youtube.com/watch?v="+search_results[0])
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    async with ctx.typing():
-        player = await YTDLSource.from_url(search_results[0], loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-    await ctx.send('**Now playing:** {}'.format(player.title))
-
-@tasks.loop(seconds=20)
-async def change_status():
-    await client.change_presence(activity=discord.Game(choice(status)))
 bot.run('TOKEN')
+
 
 
